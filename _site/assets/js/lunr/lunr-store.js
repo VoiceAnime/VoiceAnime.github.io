387f75a831bc802e0938062b58850917d0202de8
kayanoma <kayaryou@icloud.com>
2020-03-29 08:59:10 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "移設！",
-        "excerpt":"github-pages+jekyllで作ったページに移設！  ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/docs/jekyll/update/2020/03/21/welcome-to-jekyll.html",
-        "teaser": null
-      }]
